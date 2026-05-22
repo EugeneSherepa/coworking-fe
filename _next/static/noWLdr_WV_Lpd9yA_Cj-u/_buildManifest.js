c83605a64a5bc4c20d08911d8a49410402b04cd1
Eugene Sherepa <evgenijserepa@gmail.com>
2026-05-22 16:51:11 +0300

    Updates

--- _next/static/noWLdr_WV_Lpd9yA_Cj-u/_buildManifest.js
+++ _next/static/noWLdr_WV_Lpd9yA_Cj-u/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/coworking-fe//_next/:path+",
+        "destination": "/coworking-fe/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
